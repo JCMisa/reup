1b84b1107411ef07ce086625953f74b110fa533b
MisaJC <johncarlomisa399@gmail.com>
2025-07-19 09:57:45 +0800

    design the auth pages

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,22 @@
+import { BackgroundLines } from "@/components/ui/background-lines";
+import { SignIn } from "@clerk/nextjs";
+
 const SigninPage = () => {
-  return <div>SigninPage</div>;
+  return (
+    <div>
+      <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
+        <div className="mt-[40rem] md:mt-0 flex flex-col gap-4 items-center justify-center">
+          <SignIn />
+
+          <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
+            Welcome back to ReUp! Ready to continue perfecting your resume? Sign
+            in to access your progress and powerful AI feedback. Let&apos;s get
+            you one step closer to your dream job!
+          </p>
+        </div>
+      </BackgroundLines>
+    </div>
+  );
 };
 
 export default SigninPage;
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,21 @@
+import { BackgroundLines } from "@/components/ui/background-lines";
+import { SignUp } from "@clerk/nextjs";
+
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  return (
+    <div>
+      <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
+        <div className="mt-[40rem] md:mt-0 flex flex-col gap-4 items-center justify-center">
+          <SignUp />
+          <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
+            Ready to craft a standout resume and land your dream job? Sign up
+            for ReUp today and unlock our intuitive builder and instant AI
+            feedback. Your future starts now!
+          </p>
+        </div>
+      </BackgroundLines>
+    </div>
+  );
 };
 
 export default SignupPage;
